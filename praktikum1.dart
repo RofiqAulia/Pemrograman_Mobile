1c379181bfdd3ecaac40e0364c5cea804f37cf5b
= <=>
2024-09-10 09:51:38 +0700

    first commit

---
+++ praktikum1.dart
@@ -0,0 +1,23 @@
+// Pengantar Bahasa Pemrogaman Dart - Bagian 1
+
+// Soal 1
+void main() {
+  for (int i = 20; i > 10; i--) {
+    print('Nama saya adalah Fulan, sekarang berumur ${i-2}');
+  }
+}
+
+// Soal 2
+// Mengapa sangat penting untuk memahami bahasa pemrograman Dart sebelum kita menggunakan framework Flutter ? Jelaskan!
+// Jawaban: Bahasa pemrograman Dart sangat penting untuk dipahami sebelum menggunakan 
+// framework Flutter karena Dart adalah bahasa pemrograman yang digunakan untuk mengembangkan aplikasi
+// Flutter. Dengan memahami bahasa pemrograman Dart, kita dapat membuat aplikasi
+// yang lebih kompleks dan interaktif dengan lebih mudah.
+
+// Soal 3
+// Rangkumlah materi dari codelab ini menjadi poin-poin penting yang dapat Anda gunakan untuk membantu proses pengembangan aplikasi mobile menggunakan framework Flutter.
+// 1. Untuk mempermudah pemahaman di pemrograman mobile dengan bahasa dart dan framework flutter diperlukan pengetahuan di algoritma pemrograman dasar dann pemorgraman berorientasi objek
+// 2. Pemahaman dasar-dasar bahasa pemrograman Dart dan framework
+// 3. Pemahaman dasar-dasar pengembangan aplikasi mobile dengan framework
+// 4. Pemahaman dasar-dasar penggunaan widget dan layout dalam framework
+// 5. Pemahaman dasar-dasar penggunaan state dan lifecycle dalam framework
